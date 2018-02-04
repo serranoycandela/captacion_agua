ee6f21e74228bbf5e4b98fb9e64f2ad5b7badf80
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-02-03 18:10:11 -0600

    docstrings for classes

--- model.py
+++ model.py
@@ -6,6 +6,14 @@ from pony.orm import *
 db = Database()
 
 class Block(db.Entity):
+    """
+    Model of a city block.
+
+    Blocks have surfaces that collect rainwater in their tanks.
+    Tanks fill up with this water, and drain with usage by population.
+
+    For any given day the model reports usage of water from the tank.
+    """
     id = PrimaryKey(int, auto=True)    
     population = Required(Decimal)
     use = Required(Decimal)
@@ -20,6 +28,10 @@ class Block(db.Entity):
         pass
 
 class Precipitation(db.Entity):
+    """
+    For a given block and a given day, how much rain from the sky has
+    fallen?
+    """
     block = Required('Block')
     day = Required(date)
     volume = Required(Decimal)    
