c9f86b043667f9edec69817c8f3260ea65bff0fc
Fidel Serrano <serranoycandela@gmail.com>
2018-03-27 20:44:24 -0600

    fix bug in cosumo

--- qgis_script.py
+++ qgis_script.py
@@ -19,15 +19,15 @@ numeroDeDias = 361
 folder = dirname(shape_path)
 
 print folder
- 
+
 manzana_layer = QgsVectorLayer(shape_path, "manzanas", "ogr")
-    
+
 w_stored_array = np.zeros(numeroDeDias+1)
 sum_used_array = np.zeros(numeroDeDias+1)
 
 #para corerlo varios anios seguidos
 #for manzana in manzana_layer.getFeatures():
-#    w_stored_array[0] += manzana['w_stored'] 
+#    w_stored_array[0] += manzana['w_stored']
 
 manzana_layer.startEditing()
 
@@ -44,19 +44,20 @@ for manzana in manzana_layer.getFeatures():
     n_inst = math.trunc(manzana['area_p'] / areaCaptacion)
     if manzana['viv_hab'] < n_inst:
         n_inst = manzana['viv_hab']
-    
+
     if manzana['ocup_viv'] == 0:
         n_inst = 0
-    
+
     manzana['n_inst'] = n_inst
-    
-    
-    
+
+
+    manzana['sum_used'] = 0
+    manzana['w_stored'] = 0
     manzana['tank_cap'] = n_inst * volumenCisterna
-    manzana['consumo'] = n_inst * consumoPorPersona
+    manzana['consumo'] = n_inst * manzana['prom_ocup'] * consumoPorPersona
     manzana['area_c'] = n_inst * areaCaptacion
     manzana_layer.updateFeature(manzana)
-        
+
 
 manzana_layer.commitChanges()
 
@@ -64,7 +65,7 @@ manzana_layer.commitChanges()
 print "raining a year..."
 
 manzana_layer.startEditing()
-contador = 0   
+contador = 0
 
 for manzana in manzana_layer.getFeatures():
     #si no hay instalaciones en esta manzana no calcula nada
@@ -72,51 +73,51 @@ for manzana in manzana_layer.getFeatures():
     if contador % 6300 == 0:
         print str(contador / 630)+ "%"
     if manzana['n_inst'] > 0:
-        water_stored = manzana['w_stored'] 
+        water_stored = manzana['w_stored']
         sum_used = 0
         surface_used = manzana['area_c']
         manzanaCapacityTank = manzana['tank_cap']
         consumo = manzana['consumo']
-        
+
         #print manzana['CVEGEO'], contador
-        
+
         for day in range(1,numeroDeDias+1):
-            
+
             lluviaField = "p" + str(day).zfill(3) + "mean"
-          
+
             precipitacion = manzana[lluviaField]
-            
-            #el agua que callo este dia en la superficie de captacion 
-            #en litros pues la precipitacion viene en milimetros y la surface_used en 
+
+            #el agua que callo este dia en la superficie de captacion
+            #en litros pues la precipitacion viene en milimetros y la surface_used en
             #metros cuadrados, para un metro cuadrado un milimetro es un litro
-            water_today = factorEficiencia * factorMaligno * (precipitacion * surface_used) 
-    
+            water_today = factorEficiencia * factorMaligno * (precipitacion * surface_used)
+
             #se usa lo que hay y no mas
-            sum_used += min(water_stored,consumo) 
+            sum_used += min(water_stored,consumo)
 
             #se acumula lo que cae menos lo que se usa, con los limites entre 0 y manzanaCapacityTank
             water_stored = max(0, min(manzanaCapacityTank, water_stored + water_today - consumo))
-           
+
             #estos arrays obtienen la suma para todas las manzanas del agua acumulada : w_stored y del agua aprovechada: sum_used para cada dia del anio
             sum_used_array[day] += sum_used
             w_stored_array[day] += water_stored
-            
+
         manzana['sum_used'] = sum_used
         manzana['w_stored'] = water_stored
         manzana_layer.updateFeature(manzana)
-        
+
 
 manzana_layer.commitChanges()
 try:
     with open(join(folder,'sum_used_array.json'), 'w') as outfile:
         json.dump(list(sum_used_array), outfile)
-        
+
     with open(join(folder,'w_stored_array.json'), 'w') as outfile:
         json.dump(list(w_stored_array), outfile)
-        
+
 except:
     print "no pude"
-        
+
 
 contador = 0
 print "calculating percentage of comsumption from rain..."
@@ -125,13 +126,13 @@ for manzana in manzana_layer.getFeatures():
     contador += 1
     if contador % 6300 == 0:
         print str(contador / 630)+ "%"
-    consumoAnual = numeroDeDias*consumoPorPersona*manzana['ocup_viv'] 
+    consumoAnual = numeroDeDias*consumoPorPersona*manzana['ocup_viv']
     if consumoAnual > 0:
         manzana['p_lluvia'] = 100 * (manzana['sum_used'] / consumoAnual)
     else:
         manzana['p_lluvia'] = 0
-        
+
     manzana_layer.updateFeature(manzana)
-        
+
 
 manzana_layer.commitChanges()
